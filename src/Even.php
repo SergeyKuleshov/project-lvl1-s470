6009a5bed6b2793eac325e00e56eeeccd100f0d8
Sergei Kuleshov <kilinserg89@mail.ru>
2019-04-05 11:18:20 +0300

    Created game brain-even in the src/Even.php

---
+++ src/Even.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BrainGamesProject\Even;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    line('Answer "yes" if number even otherwise answer "no".');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    
+    $numbers = [
+        'firstNumber' => 15,
+        'secondNumber' => 6,
+        'thirdNumber' => 7
+    ];
+
+    $count = 0;
+    foreach ($numbers as $num) {
+        line("Question: " . $num);
+        $answer = prompt("Even?");
+        line("Your answer " . $answer);
+
+        if ( ($num % 2 == 0 && $answer == 'yes') || ($num % 2 != 0 && $answer == 'no') ) {
+            line("Correct");
+            $count++;
+        }
+    }
+
+    if ($count == 3) {
+        line("Congratulations," . $name);
+    }
+
+    line($count);
+}
